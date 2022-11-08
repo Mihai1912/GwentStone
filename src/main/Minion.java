0fa8b6d97de71bc49bdfb5d8551195eb95b0b475
Mihai1912 <89154289+Mihai1912@users.noreply.github.com>
2022-11-08 19:35:23 +0200

    Add

---
+++ src/main/Minion.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Minion {
+    int mana;
+    int health;
+    int attackDamage;
+    String description;
+    ArrayList<String> colors;
+    String name;
+}
